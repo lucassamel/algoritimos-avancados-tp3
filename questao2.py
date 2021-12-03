4544ec04142698aa2dedeb312c40b23371418fe7
Lucas Samel <lucas.samel97@gmail.com>
2021-12-03 17:15:02 -0300

    Algoritimo de Kruskal

--- questao2.py
+++ questao2.py
@@ -5,6 +5,7 @@ n, m = int(input().split())
 H = []
 
 for j in range(m):
+    # b = peso da aresta
     a, b, c = int(input().split())
     heapq.heappush(H, (c, a, b))
 
@@ -18,3 +19,29 @@ S = []
 
 for i in range(n):
     S.append(i)
+
+cont = 0
+custo = 0
+
+while cont < n - 1:
+    c, a, b = heapq.heappop(H)
+
+    if S[a] != S[b]:
+        custo = custo + c
+
+        p = S[a]
+        q = S[b]
+
+        # Troca o indice
+        if q < p:
+            p, q = q, p
+
+        for j in C[q]:
+            S[j] = p
+
+        C[p].extend(C[q])
+        C[q] = []
+        cont = cont + 1
+
+        
+print(custo)
